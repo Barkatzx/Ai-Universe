0aaae1f7c94f77088ee16e02254d20faf0d5a67f
Barkat Ullah <barkatullah.zx@gmail.com>
2023-03-03 04:50:03 +0600

    Modal Added

--- script.js
+++ script.js
@@ -28,11 +28,49 @@ const displayAi = aiArray => {
                     <p class="font-semibold"><i class="fa-regular fa-calendar-days"></i> ${ai.published_in}</p>
                     </div>
                     <div class="card-actions text-3xl">
-                    <i class="fa-solid fa-circle-arrow-right"></i>
+                    <label for="my-modal-5">
+                    <i class="fa-solid fa-circle-arrow-right"></i></label>
+                
                     </div>
                     </div>
                 </div>
             </div>
+
+    <input type="checkbox" id="my-modal-5" class="modal-toggle" />
+        <div class="modal">
+        <div class="modal-box w-11/12 max-w-5xl">
+         <label for="my-modal-5" class="btn btn-sm btn-circle absolute right-1 top-1">✕</label>
+         <div class="flex gap-12 hove:box-border">
+        <div class="bg-rose-300 rounded-lg p-6">
+            <h1 class="font-bold text-5xl">Here Is Title</h1>
+            <div class="flex gap-12 mt-5 items-center">
+                <div class="bg-slate-100 box-border h-32 w-32 p-4 rounded-lg text-green-600 text-2xl font-bold text-center">$ 10/ month basic</div>
+                <div class="bg-slate-100 box-border h-32 w-32 p-4 rounded-lg text-green-600 text-2xl font-bold text-center">67</div>
+                <div class="bg-slate-100 box-border h-32 w-32 p-4 rounded-lg text-green-600 text-2xl font-bold text-center">67</div>
+            </div>
+            <div class="flex gap-12 mt-5 text-center">
+                <div>
+                <h1 class="text-3xl font-bold">Features</h1>
+                <p>. bangladesh</p>
+                </div>
+                <div>
+                <h1 class="text-3xl font-bold">Integrations</h1>
+                <p>. bangladesh</p>
+                </div>
+            </div>
+        </div>
+        <div class="card w-96 bg-base-100 shadow-xl">
+            <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <div class="card-body">
+              <h2 class="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div class="card-actions justify-end">
+              </div>
+            </div>
+          </div>
+    </div>
+  </div>
+</div>
         `;
         aiContainer.appendChild(aiDiv);
     });
